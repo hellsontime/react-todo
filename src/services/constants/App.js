0e5ca89925760dfb95ad4137174ca620aba4d429
Raman Kanapeshka <raman.kanapeshka@innowise-group.com>
2022-06-01 13:16:33 +0300

    Definig api routes

---
+++ src/services/constants/App.js
@@ -0,0 +1,7 @@
+export const API_URL = process.env.REACT_APP_API_HOST
+  ? process.env.REACT_APP_API_HOST + process.env.REACT_APP_API_BASE_PATH
+  : 'http://localhost:8081/'
+
+export const TODOS_API_ROUTE = '/todos/'
+export const STATUS_API_ROUTE = '/status/'
+export const LOGOUT_API_ROUTE = '/logout'
