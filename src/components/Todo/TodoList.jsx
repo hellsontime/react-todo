ae80ee1ce8bd55ba51c862e134d18f7626321081
Raman Kanapeshka <raman.kanapeshka@innowise-group.com>
2022-06-02 14:25:01 +0300

    TodoList refactor to work with API via axios

--- src/components/Todo/TodoList.jsx
+++ src/components/Todo/TodoList.jsx
@@ -1,42 +1,61 @@
 /* eslint-disable no-unused-vars */
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
+import { TODOS_API_ROUTE } from '../../services/constants/App'
+import { customAxios } from '../../services/helpers/axiosHelper'
+import createTodoHelper from '../../services/helpers/todos/createTodoHelper'
+import deleteTodoHelper from '../../services/helpers/todos/deleteTodoHelper'
+import updateTodoStatusHelper from '../../services/helpers/todos/status/updateTodoStatusHelper'
+import updateTodoHeper from '../../services/helpers/todos/updateTodoHelper'
 import Todo from './Todo'
 import TodoForm from './TodoForm'
-import { CloseOutlined } from '@ant-design/icons'
 
 export default function TodoList() {
   const [todos, setTodos] = useState([])
 
   const addTodo = (todo) => {
-    if (!todo.text || /^\s*$/.test(todo.text)) {
+    if (!todo.title || /^\s*$/.test(todo.title)) {
       return
     }
 
-    setTodos([todo, ...todos])
+    createTodoHelper(todo.title, setTodos)
   }
 
-  const updateTodo = (todoId, newValue) => {
-    if (!newValue.text || /^\s*$/.test(newValue.text)) {
+  const updateTodo = (updatedTodo, setEdit, setInput) => {
+    if (!updatedTodo.title || /^\s*$/.test(updatedTodo.title)) {
       return
     }
 
-    setTodos((prevState) => prevState.map((item) => (item.id === todoId ? newValue : item)))
+    updateTodoHeper(updatedTodo, setTodos, setEdit, setInput)
   }
 
   const removeTodo = (id) => {
-    let removedArr = [...todos].filter((todo) => todo.id !== id)
-    setTodos(removedArr)
+    deleteTodoHelper(id, setTodos)
   }
 
+  console.log(todos)
+
   const completeTodo = (id) => {
-    let updatedTodos = todos.map((todo) => {
-      if (todo.id === id) {
-        todo.isComplete = !todo.isComplete
+    todos.map((todo) => {
+      if (todo.id == id) {
+        todo.status === 1
+          ? updateTodoStatusHelper(id, 0, setTodos)
+          : updateTodoStatusHelper(id, 1, setTodos)
       }
-      return todo
     })
-    setTodos(updatedTodos)
   }
+
+  useEffect(() => {
+    async function fetchTodos() {
+      try {
+        let res = await customAxios.get(TODOS_API_ROUTE)
+        setTodos(res.data)
+      } catch (err) {
+        console.log(err)
+      }
+    }
+
+    fetchTodos()
+  }, [])
 
   const finishUserSession = () => {
     localStorage.clear()
@@ -46,13 +65,13 @@ export default function TodoList() {
     <>
       <div className="todo-app">
         <a onClick={finishUserSession} target="_self" rel="noopener noreferrer" href="/">
-          {/* <CloseOutlined className="logout-button" onClick={finishUserSession} /> */}
           <div className="logout-button">&times;</div>
         </a>
         <h1>What`s a plan for today?</h1>
         <TodoForm onSubmit={addTodo} />
         <Todo
           todos={todos}
+          setTodos={setTodos}
           completeTodo={completeTodo}
           removeTodo={removeTodo}
           updateTodo={updateTodo}
