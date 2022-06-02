ac64762c0dc2a2ed700a66d390cb43eeef28f350
Raman Kanapeshka <raman.kanapeshka@innowise-group.com>
2022-06-02 14:26:06 +0300

    Todo component refactor to integrate with API

--- src/components/Todo/Todo.jsx
+++ src/components/Todo/Todo.jsx
@@ -3,20 +3,13 @@ import PropTypes from 'prop-types'
 import { RiCloseCircleLine } from 'react-icons/ri'
 import { TiEdit } from 'react-icons/ti'
 import TodoForm from './TodoForm'
+import emptyTodo from '../../services/constants/EmptyTodo'
 
 export default function Todo({ todos, completeTodo, removeTodo, updateTodo }) {
-  const [edit, setEdit] = useState({
-    id: null,
-    value: ''
-  })
+  const [edit, setEdit] = useState(emptyTodo)
 
-  const submitUpdate = (value) => {
-    console.log(value)
-    updateTodo(edit.id, value)
-    setEdit({
-      id: null,
-      value: ''
-    })
+  const submitUpdate = (updatedTodo, setInput) => {
+    updateTodo(updatedTodo, setEdit, setInput)
   }
 
   if (edit.id) {
@@ -24,13 +17,16 @@ export default function Todo({ todos, completeTodo, removeTodo, updateTodo }) {
   }
 
   return todos.map((todo, index) => (
-    <div className={todo.isComplete ? 'todo-row complete' : 'todo-row'} key={index}>
-      <div key={todo.id} onClick={() => completeTodo(todo.id)}>
-        {todo.text}
+    <div className={todo.status === 1 ? 'todo-row complete' : 'todo-row'} key={index}>
+      <div className="todo-row__title" key={todo.id} onClick={() => completeTodo(todo.id)}>
+        {todo.title}
       </div>
       <div className="icons">
         <RiCloseCircleLine onClick={() => removeTodo(todo.id)} className="delete-icon" />
-        <TiEdit onClick={() => setEdit({ id: todo.id, value: todo.text })} className="edit-icon" />
+        <TiEdit
+          onClick={() => setEdit({ id: todo.id, title: todo.title, status: todo.status })}
+          className="edit-icon"
+        />
       </div>
     </div>
   ))
