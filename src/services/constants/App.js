792add3d3377edb7f68a1f86ecf2d89bf6b60ef5
Raman Kanapeshka <raman.kanapeshka@innowise-group.com>
2022-06-02 14:22:35 +0300

    App constant routes fix

--- src/services/constants/App.js
+++ src/services/constants/App.js
@@ -1,7 +1,7 @@
 export const API_URL = process.env.REACT_APP_API_HOST
   ? process.env.REACT_APP_API_HOST + process.env.REACT_APP_API_BASE_PATH
-  : 'http://localhost:8081/'
+  : 'http://todo/api/v1'
 
-export const TODOS_API_ROUTE = '/todos/'
-export const STATUS_API_ROUTE = '/status/'
+export const TODOS_API_ROUTE = '/todos'
+export const STATUS_API_ROUTE = '/status'
 export const LOGOUT_API_ROUTE = '/logout'
