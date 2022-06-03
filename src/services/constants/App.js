9c438e8072b94f07e1ee3730530edb94d4e967c1
Raman Kanapeshka <raman.kanapeshka@innowise-group.com>
2022-06-03 11:22:10 +0300

    change default api url

--- src/services/constants/App.js
+++ src/services/constants/App.js
@@ -1,6 +1,6 @@
 export const API_URL = process.env.REACT_APP_API_HOST
   ? process.env.REACT_APP_API_HOST + process.env.REACT_APP_API_BASE_PATH
-  : 'http://todo/api/v1'
+  : 'http://localhost:8081/api/v1'
 
 export const TODOS_API_ROUTE = '/todos'
 export const STATUS_API_ROUTE = '/status'
